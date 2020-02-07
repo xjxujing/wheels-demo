c908fe05f24c9ae9f5b06c3f78ec4a90eb89eaa2
myName <xujing_xj@foxmail.com>
2020-02-07 16:09:59 +0800

    布局组件基本功能完成

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import Button from "./button.vue";
 import ButtonGroup from "./button-group.vue";
 import Icon from "./Icon";
 import Input from "./input";
-import Row from "./row"
-import Col from "./col"
+import Row from "./row";
+import Col from "./col";
+import Layout from "./layout";
+import Header from "./header";
+import Content from "./content";
+import Footer from "./footer";
+import Sider from "./sider";
 
 import chai from "chai";
 import spies from "chai-spies";
@@ -14,8 +19,13 @@ Vue.component("gulu-button", Button);
 Vue.component("gulu-button-group", ButtonGroup);
 Vue.component("gulu-icon", Icon);
 Vue.component("gulu-input", Input);
-Vue.component("gulu-row", Row)
-Vue.component("gulu-col",   Col)
+Vue.component("gulu-row", Row);
+Vue.component("gulu-col", Col);
+Vue.component("gulu-layout", Layout);
+Vue.component("gulu-header", Header);
+Vue.component("gulu-content", Content);
+Vue.component("gulu-footer", Footer);
+Vue.component("gulu-sider", Sider);
 
 new Vue({
     el: "#app",
