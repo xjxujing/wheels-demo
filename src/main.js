9d6c204383bdff4e0edeba452ba1bafbf517a12d
myName <xujing_xj@foxmail.com>
2020-02-20 14:18:25 +0800

    popover 最基础的功能

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import TabsHead from "./tabs-head";
 import TabsBody from "./tabs-body";
 import TabsItem from "./tabs-item";
 import TabsPane from "./tabs-pane";
+import Popover from "./popover"
 
 Vue.use(plugin);
 
@@ -37,6 +38,7 @@ Vue.component("gulu-tabs-head", TabsHead);
 Vue.component("gulu-tabs-body", TabsBody);
 Vue.component("gulu-tabs-item", TabsItem);
 Vue.component("gulu-tabs-pane", TabsPane);
+Vue.component("gulu-popover", Popover);
 
 new Vue({
     el: "#app",
