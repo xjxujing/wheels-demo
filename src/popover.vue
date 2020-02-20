e1af4c1e70dbdbc7adf43a3c97c567b9c28c8251
myName <xujing_xj@foxmail.com>
2020-02-20 20:49:43 +0800

    完善 popover 组件基础功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,50 +1,70 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
     name: "GuluPopover",
-
     data() {
-        return {
-            visible: false
-        };
+        return { visible: false };
     },
-
     methods: {
-        xxx() {
-            this.visible = !this.visible;
-            if (this.visible === true) {
-                this.$nextTick(() => {
-                    let eventHandler = () => {
-                        this.visible = false;
-                        document.removeEventListener("click", eventHandler);
-                    };
-                    document.addEventListener("click", eventHandler);
-                });
+        positionContent() {
+            document.body.appendChild(this.$refs.contentWrapper);
+            let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
+            this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+            this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+        },
+        onClickDocument(e) {
+            if (
+                this.$refs.popover &&
+                (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+            ) {
+                return;
+            }
+            this.close();
+        },
+        open() {
+            this.visible = true;
+            this.$nextTick(() => {
+                this.positionContent();
+                document.addEventListener("click", this.onClickDocument);
+            });
+        },
+        close() {
+            this.visible = false;
+            document.removeEventListener("click", this.onClickDocument);
+        },
+        onClick(event) {
+            if (this.$refs.triggerWrapper.contains(event.target)) {
+                if (this.visible === true) {
+                    this.close();
+                } else {
+                    this.open();
+                }
             }
         }
     }
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+}
+.content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
 }
 </style>
