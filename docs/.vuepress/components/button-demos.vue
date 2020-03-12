a8277d113748d73d9df25a56f25784faa56c6cbd
许静 <xujing_xj@foxmail.com>
2020-03-12 16:23:49 +0800

    使用 vuepress 写文档

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <gulu-button>默认按钮</gulu-button>
+    <gulu-button icon="settings">默认按钮</gulu-button>
+    <gulu-button :loading="true">默认按钮</gulu-button>
+    <gulu-button disabled>默认按钮</gulu-button>
+    <pre>
+      <code>{{ content }}</code>
+    </pre>
+  </div>
+</template>
+
+<script>
+import Button from "../../../src/button";
+export default {
+  name: "ButtonDemos",
+  components: {
+    "gulu-button": Button
+  },
+  data() {
+    return {
+      content: `
+      <gulu-button>默认按钮</gulu-button>
+      <gulu-button icon="settings">默认按钮</gulu-button>
+      <gulu-button :loading="true">默认按钮</gulu-button>
+      <gulu-button disabled>默认按钮</gulu-button>
+      `
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: "我的咕噜文档",
+    description: "Just playing around",
+    themeConfig: {
+        sidebar: [
+            {
+                title: "入门",
+                children: ["/install/", "/get-started/"]
+            },
+
+            {
+                title: "组件",
+                children: ["/components/button"]
+            }
+        ]
+    }
+};
