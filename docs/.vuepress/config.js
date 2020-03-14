530488c7dfcf7acf3fa3f71afabce312f6db6ab8
myName <xujing_xj@foxmail.com>
2020-03-14 16:12:08 +0800

    文档侧边栏默认展开

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,13 @@ module.exports = {
         sidebar: [
             {
                 title: "入门",
+                collapsable: false,
                 children: ["/install/", "/get-started/"]
             },
 
             {
                 title: "组件",
+                collapsable: false,
                 children: ["/components/button"]
             }
         ]
