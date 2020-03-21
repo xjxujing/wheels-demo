a01c77f6a3ff2a7d0261c9808823365d10885230
myName <xujing_xj@foxmail.com>
2020-03-21 18:00:33 +0800

    换成 vue-cli 构建项目

---
+++ src/demo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app">
+        <gulu-button icon-position="left" :loading="loading" @click="loading = !loading">
+            保存哈哈
+        </gulu-button>
+        <gulu-button
+                icon="settings"
+                icon-position="left"
+                :loading="loading"
+                @click="loading = !loading"
+        >
+            提交
+        </gulu-button>
+    </div>
+
+</template>
+
+<script>
+    import Button from "./button"
+
+    export default {
+        name: "Demo",
+        components: {
+            "gulu-button": Button
+        },
+        data() {
+            return {
+                loading: true
+            }
+        },
+    }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,76 +1,8 @@
 import Vue from "vue";
-import Button from "./button.vue";
-import ButtonGroup from "./button-group.vue";
-import Icon from "./Icon";
-import Input from "./input";
-import Row from "./row";
-import Col from "./col";
-import Layout from "./layout";
-import Header from "./header";
-import Content from "./content";
-import Footer from "./footer";
-import Sider from "./sider";
-import Toast from "./toast";
-import plugin from "./plugin";
-import Tabs from "./tabs";
-import TabsHead from "./tabs-head";
-import TabsBody from "./tabs-body";
-import TabsItem from "./tabs-item";
-import TabsPane from "./tabs-pane";
-import Popover from "./popover"
+import Demo from "./demo.vue";
 
-Vue.use(plugin);
-
-Vue.component("gulu-button", Button);
-Vue.component("gulu-button-group", ButtonGroup);
-Vue.component("gulu-icon", Icon);
-Vue.component("gulu-input", Input);
-Vue.component("gulu-row", Row);
-Vue.component("gulu-col", Col);
-Vue.component("gulu-layout", Layout);
-Vue.component("gulu-header", Header);
-Vue.component("gulu-content", Content);
-Vue.component("gulu-footer", Footer);
-Vue.component("gulu-sider", Sider);
-Vue.component("gulu-toast", Toast);
-Vue.component("gulu-tabs", Tabs);
-Vue.component("gulu-tabs-head", TabsHead);
-Vue.component("gulu-tabs-body", TabsBody);
-Vue.component("gulu-tabs-item", TabsItem);
-Vue.component("gulu-tabs-pane", TabsPane);
-Vue.component("gulu-popover", Popover);
+Vue.config.productionTip = false;
 
 new Vue({
-    el: "#app",
-    data: {
-        message: "这是 message",
-        loading: false,
-        value: "",
-        selected: "onepeace"
-    },
-    created() {},
-    methods: {
-        inputChange(e) {
-            console.log(e.target.value);
-        },
-
-        showToast(position) {
-            this.$toast("这是个 toast!", {
-                closeButton: {
-                    text: "知道了",
-                    callback(toast) {
-                        toast.log();
-                        console.log("用户知道了");
-                    }
-                },
-                position: position
-            });
-        },
-
-        yyy(e) {
-            console.log(e)
-        }
-
-    
-    }
-});
+    render: h => h(Demo)
+}).$mount("#app");
