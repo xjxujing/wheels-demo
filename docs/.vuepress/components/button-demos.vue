d80a24bf61ff08ba04a8697e29a537756c2b729f
myName <xujing_xj@foxmail.com>
2020-03-21 18:43:46 +0800

    完善 1. 用 npm 写文档报错，换成了 yarn 2. 添加了 button 使用的 demo

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,33 +1,49 @@
 <template>
   <div>
+
+    <h4>基础用法</h4>
     <gulu-button>默认按钮</gulu-button>
-    <gulu-button icon="settings">默认按钮</gulu-button>
-    <gulu-button :loading="true">默认按钮</gulu-button>
-    <gulu-button disabled>默认按钮</gulu-button>
-    <pre>
-      <code>{{ content }}</code>
-    </pre>
+    <gulu-button icon="thumbs-up">点赞</gulu-button>
+
+    <pre><code>{{ useBasic }}</code></pre>
+
+    <h4>点击按钮</h4>
+    <gulu-button
+      icon="settings"
+      icon-position="left"
+      :loading="loading"
+      @click="loading = !loading"
+    >
+      提交
+    </gulu-button>
+
+    <pre><code>{{ useEvent }}</code></pre>
+
   </div>
 </template>
 
 <script>
-import Button from "../../../src/button";
-export default {
-  name: "ButtonDemos",
-  components: {
-    "gulu-button": Button
-  },
-  data() {
-    return {
-      content: `
+  import Button from "../../../src/button"
+
+  export default {
+    name: "ButtonDemos",
+    components: {
+      "gulu-button": Button
+    },
+    data() {
+      return {
+        useBasic: `
       <gulu-button>默认按钮</gulu-button>
-      <gulu-button icon="settings">默认按钮</gulu-button>
-      <gulu-button :loading="true">默认按钮</gulu-button>
-      <gulu-button disabled>默认按钮</gulu-button>
-      `
-    };
+      <gulu-button icon="thumbs-up">点赞</gulu-button>
+      `.replace(/\t+| +/g, "").trim(),
+
+        useEvent: `
+        <gulu-button icon="settings" icon-position="left" :loading="loading" @click="loading = !loading">提交</gulu-button>
+        `.replace(/\t+| +/g, " ").trim(),
+        loading: false
+      }
+    }
   }
-};
 </script>
 
 <style>
